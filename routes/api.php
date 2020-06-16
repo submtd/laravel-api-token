3404f011aaae3443dcac06b26a205f9910483036
Steve Harmeyer <harmeyersteve@gmail.com>
2020-06-16 09:15:58 -0700

    added api to public routes. named endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,25 +4,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => 'api/v1/token',
+    'middleware' => 'api',
     'namespace' => 'Submtd\LaravelApiToken\Controllers',
 ], function () {
     /*
      * Public routes
      */
-    Route::post('login', 'LoginForToken');
-    Route::post('refresh', 'RefreshToken');
+    Route::post('login', 'LoginForToken')->name('api-token.login');
+    Route::post('refresh', 'RefreshToken')->name('api-token.refresh');
     /*
      * Protected routes
      */
     Route::group([
-        'middleware' => [
-            'api',
-            'auth:api',
-        ],
+        'middleware' => 'auth:api',
     ], function () {
-        Route::get('/', 'ListTokens');
-        Route::post('create', 'CreateToken');
-        Route::get('{uuid}', 'ShowToken');
-        Route::delete('{uuid}', 'DestroyToken');
+        Route::get('/', 'ListTokens')->name('api-token.list');
+        Route::post('create', 'CreateToken')->name('api-token.create');
+        Route::get('{uuid}', 'ShowToken')->name('api-token.show');
+        Route::delete('{uuid}', 'DestroyToken')->name('api-token.destroy');
     });
 });
